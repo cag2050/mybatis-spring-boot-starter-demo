d61ece3f0b0d1a77174232934f0dbee12a80dd21
cag2050 <cag2050@163.com>
2017-11-06 16:21:11 +0800

    m

--- src/main/java/com/cag/starter/MybatisSpringBootStarterDemoApplication.java
+++ src/main/java/com/cag/starter/MybatisSpringBootStarterDemoApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication //等价于 @Configuration + @EnableAutoConfiguration + @ComponentScan
 @MapperScan("com.cag.mapper")
 public class MybatisSpringBootStarterDemoApplication {
 
