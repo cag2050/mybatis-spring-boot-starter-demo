11e7b41e5326ead4ba6afa032a487c32afb356fb
cag2050 <cag2050@163.com>
2017-11-08 09:51:34 +0800

    m

--- src/test/java/com/cag/starter/MybatisSpringBootStarterDemoApplicationTests.java
+++ src/test/java/com/cag/starter/MybatisSpringBootStarterDemoApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Iterator;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
